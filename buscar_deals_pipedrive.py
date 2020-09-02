8a18c8aeb5159b5362d35d4ffef1690b6fcb7f4f
M. Sabadini <m.sabadini@avanade.com>
2020-09-02 15:50:38 -0300

    Initial commit

---
+++ buscar_deals_pipedrive.py
@@ -0,0 +1,41 @@
+from pipedrive import Pipedrive
+import sys
+import pandas as pd
+
+API_KEY   = ''
+pipedrive = Pipedrive(API_KEY)
+df        = pd.DataFrame(columns=('add_time', 'lead_id', 'person_name', 'org_name', 'origem', 'person_id_phone', 'person_id_mail', 'owner_name', 'status',  'stage_change_time' ,  'lost_reason' ,'pipeline_id'))
+
+final_lines = []
+for st in range(0,1000000,100):    
+    # Faz a chamada da API
+    results = pipedrive.deals({'start': st,}, method='GET')
+    # se tiver resultado, vai 
+    if results['data'] != None:       
+        for r in results['data']:            
+            if r['person_id'] != None and type(r['person_id']) != int:
+                phone = r['person_id']['phone'][0]['value']
+                email = r['person_id']['email'][0]['value']
+            else :
+                phone = ''
+                email = ''
+            
+            df = df.append([{
+                'add_time': r['add_time'], 
+                'lead_id': r['id'], 
+                'person_name': r['person_name'], 
+                'org_name': r['org_name'], 
+                'origem': '', 
+                'person_id_phone': phone, 
+                'person_id_mail': email, 
+                'owner_name': r['owner_name'], 
+                'status': r['status'], 
+                'stage_change_time' : r['stage_change_time'], 
+                'lost_reason' : r['lost_reason'], 
+                'pipeline_id': r['pipeline_id'] 
+            }], ignore_index=True)            
+    else:
+        # Quando os resultados da API acabarem ele encerra o loop
+        break
+
+df.to_csv('deals.csv', index=False)
